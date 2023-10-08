43122da125bd9a087c072a4536df93ca76db5037
unknown <rislammb@gmail.com>
2023-10-08 15:07:47 +0600

    some update for english to bengali laguage

--- utils/committee.js
+++ utils/committee.js
@@ -11,12 +11,15 @@ const validatePostCommittee = ({
   const newCommittee = {};
 
   if (!committeeTitle) {
-    error.committeeTitle = 'Committee Title must not be empty!';
+    // error.committeeTitle = 'Committee Title must not be empty!';
+    error.committeeTitle = 'কমিটির শিরোনাম থাকতে হবে!';
   } else if (typeof committeeTitle !== 'string') {
-    error.committeeTitle = 'Committee Title type must be string!';
+    // error.committeeTitle = 'Committee Title type must be string!';
+    error.committeeTitle = 'কমিটির শিরোনামের ধরন স্ট্রিং হতে হবে!';
   } else if (committeeTitle.trim().length < 5) {
-    error.committeeTitle =
-      'Committee Title must be at least 5 characters long!';
+    // error.committeeTitle =
+    //   'Committee Title must be at least 5 characters long!';
+    error.committeeTitle = 'কমিটির শিরোনাম কমপক্ষে ৫ অক্ষর দীর্ঘ হতে হবে!';
   } else {
     newCommittee.committeeTitle = committeeTitle.trim();
     newCommittee.committeePath = committeeTitle
@@ -26,30 +29,36 @@ const validatePostCommittee = ({
   }
 
   if (!indexNumber) {
-    newCommittee.indexNumber = '';
+    newCommittee.indexNumber = '99';
   } else if (typeof indexNumber !== 'string') {
-    error.indexNumber = 'Committee Index Number type must be string!';
+    // error.indexNumber = 'Committee Index Number type must be string!';
+    error.indexNumber = 'কমিটির ইনডেক্স সংখার ধরন স্ট্রিং হতে হবে!';
   } else if (/^\d+$/.test(indexNumber.trim()) === false) {
-    error.indexNumber = 'Committee Index Number characters must be digit!';
+    // error.indexNumber = 'Committee Index Number characters must be digit!';
+    error.indexNumber = 'কমিটির ইনডেক্স সংখ্যা অংকের সংখ্যা হতে হবে!';
   } else {
     newCommittee.indexNumber = indexNumber.trim();
   }
 
   if (!workHasStarted) {
-    error.workHasStarted = 'Committee work has start must not be empty!';
+    // error.workHasStarted = 'Committee work has start must not be empty!';
+    error.workHasStarted = 'কমিটির কার্যক্রম শুরুর তারিখ থাকতে হবে!';
   } else if (
     typeof workHasStarted !== 'string' ||
     workHasStarted.length !== 24
   ) {
-    error.workHasStarted = 'Committee work has started is not valid date!';
+    // error.workHasStarted = 'Committee work has started is not valid date!';
+    error.workHasStarted = 'কমিটির কার্যক্রম শুরুর তারিখের ধরন সঠিক নয়!';
   } else {
     newCommittee.workHasStarted = workHasStarted;
   }
 
   if (!willExpire) {
-    error.willExpire = 'Committee will expire must not be empty!';
+    // error.willExpire = 'Committee will expire must not be empty!';
+    error.willExpire = 'কমিটির মেয়াদ শেষের তারিখ থাকতে হবে!';
   } else if (typeof willExpire !== 'string' || willExpire.length !== 24) {
-    error.willExpire = 'Committee will expire is not valid date!';
+    // error.willExpire = 'Committee will expire is not valid date!';
+    error.willExpire = 'কমিটির মেয়াদ শেষের তারিখের ধরন সঠিক নয়!';
   } else {
     newCommittee.willExpire = willExpire;
   }
--- utils/member.js
+++ utils/member.js
@@ -6,37 +6,48 @@ const validatePostMembers = (members) => {
   const newMembers = [];
 
   if (!members) {
-    error.main = 'Committee members list must not be empty!';
+    // error.main = 'Committee members list must not be empty!';
+    error.main = 'কমিটির সদস্যদের তালিকা থাকতে হবে!';
   } else if (!Array.isArray(members)) {
-    error.main = 'Committee member list type must be an array!';
+    // error.main = 'Committee member list type must be an array!';
+    error.main = 'কমিটির সদস্য তালিকার ধরন অবশ্যই অ্যারে হতে হবে';
   } else if (members.length < 1) {
-    error.main = 'Committee member list must not be empty!';
+    // error.main = 'Committee members list must not be empty!';
+    error.main = 'কমিটির সদস্যদের তালিকা থাকতে হবে!';
   } else {
     members.forEach((member, index) => {
       error[index] = {};
       if (typeof member !== 'object') {
-        error[index].type = 'Committee member type must be an object!';
+        // error[index].type = 'Committee member type must be an object!';
+        error[index].type = 'কমিটির সদস্যের ধরন অবজেক্ট হতে হবে!';
       }
 
       if (!member.pharmacistId) {
-        error[index].pharmacistId = 'Pharmacist ID must not be empty!';
+        // error[index].pharmacistId = 'Pharmacist ID must not be empty!';
+        error[index].pharmacistId = 'ফার্মাসিস্ট আইডি থাকতে হবে!';
       } else if (!mongoose.isValidObjectId(member.pharmacistId)) {
+        // error[index].pharmacistId =
+        //   'Pharmacist ID type must be valid object ID!';
         error[index].pharmacistId =
-          'Pharmacist ID type must be valid object ID!';
+          'ফার্মাসিস্ট আইডির ধরন সঠিক অবজেক্ট আইডি নয়!';
       }
 
       if (!member.postName) {
-        error[index].postName = 'Committee post name must not be empty!';
+        // error[index].postName = 'Committee post name must not be empty!';
+        error[index].postName = 'কমিটির পদবী থাকতে হবে!';
       } else if (typeof member.postName !== 'string') {
-        error[index].postName = 'Committee post name type must be string!';
+        // error[index].postName = 'Committee post name type must be string!';
+        error[index].postName = 'কমিটির পদবীর ধরন স্ট্রিং হতে হবে!';
       }
 
       if (!member.serialNumber) {
-        error[index].serialNumber =
-          'Committee serial number must not be empty!';
+        // error[index].serialNumber =
+        //   'Committee serial number must not be empty!';
+        error[index].serialNumber = 'ক্রমিক নেই!';
       } else if (typeof member.serialNumber !== 'string') {
-        error[index].serialNumber =
-          'Committee serial number type must be string!';
+        // error[index].serialNumber =
+        //   'Committee serial number type must be string!';
+        error[index].serialNumber = 'স্ট্রিং হবে!';
       }
 
       if (Object.keys(error[index]).length === 0) {
@@ -62,33 +73,41 @@ const validatePostMember = ({
   const newMember = {};
 
   if (!committeeId) {
-    error.committeeId = 'Committee ID must not be empty!';
+    // error.committeeId = 'Committee ID must not be empty!';
+    error.committeeId = 'কমিটির আইডি থাকতে হবে!';
   } else if (!mongoose.isValidObjectId(committeeId)) {
-    error.committeeId = 'Committee ID type must be valid object ID!';
+    // error.committeeId = 'Committee ID type must be valid object ID!';
+    error.committeeId = 'কমিটি আইডির ধরন সঠিক অবজেক্ট আইডি নয়!!';
   } else {
     newMember.committeeId = committeeId;
   }
 
   if (!pharmacistId) {
-    error.pharmacistId = 'Pharmacist ID must not be empty!';
+    // error.pharmacistId = 'Pharmacist ID must not be empty!';
+    error.pharmacistId = 'ফার্মাসিস্ট আইডি থাকতে হবে!';
   } else if (!mongoose.isValidObjectId(pharmacistId)) {
-    error.pharmacistId = 'Pharmacist ID type must be valid object ID!';
+    // error.pharmacistId = 'Pharmacist ID type must be valid object ID!';
+    error.pharmacistId = 'ফার্মাসিস্ট আইডির ধরন সঠিক অবজেক্ট আইডি নয়!';
   } else {
     newMember.pharmacistId = pharmacistId;
   }
 
   if (!postName) {
-    error.postName = 'Committee post name must not be empty!';
+    // error.postName = 'Committee post name must not be empty!';
+    error.postName = 'কমিটির পদবী থাকতে হবে!';
   } else if (typeof postName !== 'string') {
-    error.postName = 'Committee post name type must be string!';
+    // error.postName = 'Committee post name type must be string!';
+    error.postName = 'কমিটির পদবীর ধরন স্ট্রিং হতে হবে!';
   } else {
     newMember.postName = postName;
   }
 
   if (!serialNumber) {
-    error.serialNumber = 'Committee serial number must not be empty!';
+    // error.serialNumber = 'Committee serial number must not be empty!';
+    error.serialNumber = 'ক্রমিক নেই!';
   } else if (typeof serialNumber !== 'string') {
-    error.serialNumber = 'Committee serial number type must be string!';
+    // error.serialNumber = 'Committee serial number type must be string!';
+    error.serialNumber = 'স্ট্রিং হবে!';
   } else {
     newMember.serialNumber = serialNumber;
   }
