6fa17862666011930984c410d7506bd5128e8217
unknown <rislammb@gmail.com>
2023-10-09 11:57:05 +0600

    add authenticate middlewere on every route

--- routes/user.js
+++ routes/user.js
@@ -1,39 +1,40 @@
 const router = require('express').Router();
 const userController = require('../controllers/user');
+const authenticate = require('../middleware/authenticate');
 /**
  * Get user by id
  * @method GET
  */
-router.get('/:userId', userController.getUserById);
+router.get('/:userId', authenticate, userController.getUserById);
 
 /**
  * Update user by id
  * @method PUT
  */
-router.put('/:userId', userController.putUserById);
+router.put('/:userId', authenticate, userController.putUserById);
 
 /**
  * Update user by id
  * @method PATCH
  */
-router.patch('/:userId', userController.patchUserById);
+router.patch('/:userId', authenticate, userController.patchUserById);
 
 /**
  * Delete user by id
  * @method DELETE
  */
-router.delete('/:userId', userController.deleteUserById);
+router.delete('/:userId', authenticate, userController.deleteUserById);
 
 /**
  * Get all users
  * @method GET
  */
-router.get('/', userController.getUsers);
+router.get('/', authenticate, userController.getUsers);
 
 /**
  * Create new user
  * @method POST
  */
-router.post('/', userController.postUser);
+router.post('/', authenticate, userController.postUser);
 
 module.exports = router;
